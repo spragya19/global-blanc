e0cf703cb5d6f2f13f8e85579f97dfc2d0e70803
spragya19 <pragya.sharma19@icloud.com>
2022-07-30 16:20:52 +0530

    final commit

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react";
 import { Row, Container, Col } from "react-bootstrap";
 // import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-input-2";
 import { Field, Formik, Form, ErrorMessage } from "formik";
 import "../form/Form.css";
-import { Country, State, City }  from 'country-state-city';
-
-
+import { Country, State, City } from "country-state-city";
 
 import "react-phone-input-2/lib/bootstrap.css";
+import { ICity } from "country-state-city/dist/lib/interface";
 
 interface MyFormValues {
   firstName: string;
@@ -20,45 +19,41 @@ interface MyFormValues {
   address: string;
   postCode: number;
   country: string;
+}
 
-
+interface State {
+  name: string;
+  isoCode: string;
+}
+interface City {
+  name: string;
 }
 
 const Forms = () => {
-  let State = require('country-state-city').State;
-
-  const [Countries, setCountries] = useState(Country.getAllCountries())
-  const [states, setStates] = useState([]);
-    const [countr, setCountr]= useState('');
-  const initialValues = { 
-    
-    firstName: "" ,
+  const [Countries, setCountries] = useState(Country.getAllCountries());
+  const [states, setStates] = useState<State[]>([]);
+  const [countr, setCountr] = useState("");
+  const [cities, setCities] = useState<City[]>([]);
+  const initialValues = {
+    firstName: "",
     lastName: "",
-  email: "",
-  buisnessName: "",
-  phoneNumber: "",
-  address: "",
-  postCode:"",
-  country: "",
-  state: "",
-  city: "",
- };
+    email: "",
+    buisnessName: "",
+    phoneNumber: "",
+    address: "",
+    postCode: "",
+    country: "",
+    state: "",
+    city: "",
+  };
 
-
-    
-    useEffect(() => {
-        
-    }, [countr])
-
-
- console.log(Country.getAllCountries())
- console.log(State.getAllStates())
  
+
   return (
     <>
       <div>
         <Formik
-          initialValues={{...initialValues}}
+          initialValues={{ ...initialValues }}
           onSubmit={(values, { resetForm }) => {
             try {
               resetForm();
@@ -67,14 +62,23 @@ const Forms = () => {
             }
           }}
         >
-         
-          {({ errors, touched,values, isValidating, setFieldValue,handleChange,handleSubmit ,setValues}) => (
+          {({
+            errors,
+            touched,
+            values,
+            isValidating,
+            setFieldValue,
+            handleChange,
+            handleSubmit,
+            setValues,
+          }) => (
             <Form onSubmit={handleSubmit}>
               <section className="form">
                 <Container>
                   <div className="form-content-bg">
                     <Row>
                       <Col xs={12}>
+                        <h3>Global balc</h3>
                         <div className="row g-3">
                           <div className="col-sm">
                             <label htmlFor="">First Name</label>
@@ -91,7 +95,7 @@ const Forms = () => {
                             <Field
                               type="text"
                               className="form-control"
-                              name= "lastName"
+                              name="lastName"
                               placeholder="Last Name"
                               aria-label="State"
                             />
@@ -166,70 +170,131 @@ const Forms = () => {
                             <label htmlFor="">Country</label>
                             <Field
                               as="select"
-                              onChange= {(event:any)=>{
+                              onChange={(event: any) => {
                                 const country = event.target.value;
                                 setFieldValue("country", country);
-                                const selectedCountry = Countries.find(count => count.name === country);
-                                setStates(State.getAllStates()(selectedCountry?.isoCode));
-                          
+                                const selectedCountry = Countries.find(
+                                  (count) => count.name === country
+                                );
+
+                                if (selectedCountry?.isoCode) {
+                                  setCountr(selectedCountry?.isoCode);
+                                }
+
+                                if (selectedCountry?.isoCode) {
+                                  console.log(">>>>>>>", selectedCountry);
+
+                                  const statesByCountry =
+                                    State.getStatesOfCountry(
+                                      selectedCountry?.isoCode
+                                    );
+
+                                  const states = statesByCountry.map(
+                                    (item) => ({
+                                      name: item.name,
+                                      isoCode: item.isoCode,
+                                    })
+                                  );
+
+                                  setStates(states);
+                                }
                               }}
                               className="form-control"
                               placeholder="Country"
                               value={values.country}
-                             
-                              
-                          >
+                            >
+                              <option>Select Country</option>
                               {Countries.map((option, index: number) => (
-                            <option key={index} value={option.name}>
-                             {option.name}
-                            </option>
-                        ))}
-
+                                <option key={index} value={option.name}>
+                                  {option.name}
+                                </option>
+                              ))}
                             </Field>
                           </div>
                           <div className="col-sm">
                             <label htmlFor="">State</label>
                             <Field
-                            as="select"
+                              as="select"
                               type="text"
                               className="form-control"
                               placeholder="State"
                               aria-label="State"
-                              onChange={setFieldValue}
-                              name= "state"
+                              name="state"
                               values={values.state}
-                             >
-                       {states?.map((option: any, index: number) => (
-                            <option key={index} value={option.name}>
-                                {option.name}
-                            </option>
-                        ))}
-                              </Field>
+                              onChange={(event: any) => {
+                                const State = event.target.value;
+                                setFieldValue("state", State);
+
+                                const selectedState = states.find(
+                                  (count: State) => count.name === State
+                                );
+
+                                if (selectedState?.isoCode) {
+                                  console.log(
+                                    ">>>>>>>",
+                                    selectedState,
+                                    values.country
+                                  );
+                                  const citiesByStates: ICity[] =
+                                    City.getCitiesOfState(
+                                      countr,
+                                      selectedState?.isoCode
+                                    );
+
+                                  console.log(">>>>cities By", citiesByStates);
+                                  const cities = citiesByStates.map((item) => ({
+                                    name: item.name,
+                                  }));
+                                  setCities(cities);
+                                }
+                              }}
+                            >
+                              <option>Select State</option>
+                              {states?.map((option: any, index: number) => (
+                                <option key={index} value={option.name}>
+                                  {option.name}
+                                </option>
+                              ))}
+                            </Field>
                           </div>
                           <div className="col-sm">
                             <label htmlFor=""> City </label>
                             <Field
-                            as="select"
+                              as="select"
                               type="text"
                               className="form-control"
                               placeholder="City"
                               aria-label="Zip"
-                            />
+                              values={values.city}
+                              name="city"
+                              onChange={(event: any) => {
+                                const City = event.target.value;
+
+                                console.log(">>>>city", City);
+                                setFieldValue("city", City);
+                              }}
+                            >
+                              <option>Select City</option>
+                              {cities?.map((option: any, index: number) => (
+                                <option key={index} value={option.name}>
+                                  {option.name}
+                                </option>
+                              ))}
+                            </Field>
                           </div>
                         </div>
                       </Col>
-                     
                     </Row>
                     <div className="text-center mt-4">
-                    <button type="submit" className="btn btn-success ">Invest</button>
-
+                      <button type="submit" className="btn btn-success ">
+                        Invest
+                      </button>
                     </div>
                   </div>
                 </Container>
               </section>
             </Form>
           )}
-        
         </Formik>
       </div>
     </>
